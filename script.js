c31830119f93a0c45760521bb92068e5da298e88
hritam04.samanta@gmail.com <hritam04.samanta@gmail.com>
2025-08-21 02:18:03 +0530

    Basic html sccs structure

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+Shery.makeMagnet(".page1 nav .nav2 h4");
+
 function loaderPageAnimation() {
   function LoadingCounter() {
     let loadingNumber = document.querySelector(".loading-counter h4");
@@ -131,4 +133,44 @@ function loaderPageAnimation() {
     });
 }
 
-loaderPageAnimation()
+loaderPageAnimation();
+
+function locomotive(){
+  gsap.registerPlugin(ScrollTrigger);
+
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector(".main"),
+    smooth: true
+  });
+  // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+  locoScroll.on("scroll", ScrollTrigger.update);
+
+  // tell ScrollTrigger to use these proxy methods for the ".smooth-scroll" element since Locomotive Scroll is hijacking things
+  ScrollTrigger.scrollerProxy(".main", {
+    scrollTop(value) {
+      return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+    getBoundingClientRect() {
+      return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+    },
+    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+    pinType: document.querySelector(".main").style.transform ? "transform" : "fixed"
+  });
+
+  // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+  // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+  ScrollTrigger.refresh();
+}
+locomotive()
+
+// let tl = gsap.timeline()
+// tl.from(".page1 .center-text .line h1", {
+//   y: 300,
+//   stagger: 0.2,
+//   duration: 0.7,
+//   // delay: 5.3,
+//   ease: Power1,
+// });
+
